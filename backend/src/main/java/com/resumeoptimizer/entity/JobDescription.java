97862ab5a8f7bbf0bc53503e5e2ad13536e5f237
purvathota <purvathota@gmail.com>
2026-06-16 12:30:06 +0530

    code changes

--- backend/src/main/java/com/resumeoptimizer/entity/JobDescription.java
+++ backend/src/main/java/com/resumeoptimizer/entity/JobDescription.java
@@ -24,6 +24,7 @@ public class JobDescription {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @com.fasterxml.jackson.annotation.JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
